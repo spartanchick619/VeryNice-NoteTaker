f84334d3f4bed1e612d58b5622c30b82ae2304f1
Rosa Valdez <rosa.m42@icloud.com>
2023-11-23 17:11:48 -0600

    added code

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const html_routes = require('./routes/html-routes')
+const api_routes = require('./routes/api-routes')
+const PORT = process.env.PORT || 3001;
+// dynamically set the port
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static("public"));
+
+//connection to routers
+app.use(html_routes)
+app.use(api_routes)
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
